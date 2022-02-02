22585566af258bbdd87a6482479fcde9201b10d3
Gayan Malinda <73253202+GayanMalinda99@users.noreply.github.com>
2022-02-02 23:17:11 +0530

    Main Form

--- src/main/java/com/mycompany/shopping/app/Main.java
+++ src/main/java/com/mycompany/shopping/app/Main.java
@@ -10,7 +10,7 @@ package com.mycompany.shopping.app;
  */
 public class Main {
     public static void main(String[] args) {
-        System.out.println("hello world");  
+        System.out.println("hello");  
 
     }
 }
